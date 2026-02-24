c9615e0b3e6975e9cb46bb13891f19db90b28828
Mark Hopkin <mark.hopkin@elastic.co>
2026-02-24 15:10:56 +0000

    add reset to zero test

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/index.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/index.ts
@@ -15,6 +15,7 @@ export default function ({ loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./risk_score_preview_v2'));
     loadTestFile(require.resolve('./risk_scoring_task/task_execution'));
     loadTestFile(require.resolve('./risk_scoring_task/task_execution_v2'));
+    loadTestFile(require.resolve('./risk_scoring_task/reset_to_zero_v2'));
     loadTestFile(require.resolve('./risk_scoring_task/task_execution_nondefault_spaces'));
     loadTestFile(require.resolve('./risk_scoring_task/task_execution_nondefault_spaces_v2'));
     loadTestFile(require.resolve('./telemetry_usage'));
